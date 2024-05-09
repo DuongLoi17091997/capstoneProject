65a4f4a4641f1ea1b48d16f4804b2122295243a3
loi_duong <loi_duong@datahouse.com>
2024-05-09 14:18:34 +0700

    Loi: fix create admin account feature

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -9,16 +9,23 @@ use App\Models\User;
 class RegisterController extends Controller
 {
     public function register(Request $request){
-        $newUser = User::create([
+        $checkUser = User::where('email', $request->email)->first();
+        if(empty($checkUser)){
+            $newUser = User::create([
             'first_Name' => $request->first_Name,
             'last_Name' => $request->last_Name,
             'email' => $request->email,
-            'password' => bcrypt($request->password)
-        ]);
-        if(!empty($newUser)){
-            return response()->json(["code"=>"200","msg"=>"Register Successful"], 200);
+            'password' => bcrypt($request->password),
+            'type' => $request->type === "1" ? "admin" : "normal",
+            ]);
+            if(!empty($newUser)){
+                return response()->json(["code"=>"200","msg"=>"Register Successful"], 200);
+            }else{
+                return response()->json(["code"=>"400","msg"=>"Register Faild"], 400);
+            }
         }else{
-            return response()->json(["code"=>"400","msg"=>"Register Faild"], 400);
+            return response()->json(["code"=>"400","msg"=>"Username is not avaiable"], 400);
         }
+        
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'last_Name',
         'email',
         'password',
+        'type',
     ];
 
     /**
